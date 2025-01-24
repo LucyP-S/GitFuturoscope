71784ef1b77def9771849782281b07c69c46b350
LucyP-S <lpalucka@gmail.com>
2025-01-24 09:42:47 +0100

    commit with global properties as browser type

--- src/test/java/testcomponents/BaseTest.java
+++ src/test/java/testcomponents/BaseTest.java
@@ -5,56 +5,62 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import config.BrowserType;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pageobjects.LandingPage;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Properties;
 
 public class BaseTest {
     public WebDriver driver;
     public LandingPage landingPage;
 
-    public void launchBrowser(BrowserType selectedBrowser) {
-        {
-            switch (selectedBrowser) {
-                case CHROME: {
-                    WebDriverManager.chromedriver().setup();
-                    ChromeOptions options = new ChromeOptions();
-                    // options.addArguments("start-maximized");
-                    options.addArguments("user-data-dir=/Users/lucyna.palucka/Library/Application Support/Google/Chrome/autotest");
-                    driver = new ChromeDriver(options);
-                    break;
-                }
+    public WebDriver initializeDriver() throws IOException {
+        Properties prop = new Properties();
+        FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"//src//main//java//resources//GlobalData.properties");
+        prop.load(fis);
 
-                case FIREFOX: {
-                    WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
-                    break;
-                }
+        String browserName = System.getProperty("browser")!=null ? System.getProperty("browser") :prop.getProperty("browser"); // If maven command sends browser property, it will be used. If not, browser property from GlobalProperties will be used
+        // prop.getProperty("browser");
 
-                default: {
-                    WebDriverManager.chromedriver().setup();
-                    ChromeOptions options = new ChromeOptions();
-                    // options.addArguments("start-maximized");
-                    options.addArguments("user-data-dir=/Users/lucyna.palucka/Library/Application Support/Google/Chrome/autotest");
-                    driver = new ChromeDriver(options);
-                    break;
-                }
+        if (browserName.contains("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            //options.addArguments("start-maximized");
+            options.addArguments("user-data-dir=/Users/lucyna.palucka/Library/Application Support/Google/Chrome/autotest");
+            if (browserName.contains("headless")) {
+                options.addArguments("headless");
             }
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            driver.manage().window().maximize();
+            driver = new ChromeDriver(options);
+            driver.manage().window().setSize(new Dimension(1920, 1080));
         }
+        else if (browserName.equals("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+
+        }
+        else if (browserName.equals("edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+
+        }
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        return driver;
     }
 
     public List<HashMap<String, String>> getJsonDataToMap(String filePath) throws IOException {
@@ -80,7 +86,7 @@ public class BaseTest {
 
     @BeforeMethod
     public LandingPage launchApplication() throws IOException {
-        launchBrowser(BrowserType.CHROME);
+        driver = initializeDriver();
         landingPage = new LandingPage(driver);
         landingPage.goTo();
         return landingPage;
